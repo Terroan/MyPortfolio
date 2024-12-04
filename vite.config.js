41b50389943d09940c93c92ed6100c65a36ee4c4
DAJEAtSchool <daniel.jessner@htl-saalfelden.at>
2024-12-04 23:39:29 +0100

    added support for top level await

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', // Unterstützt moderne JS-Features wie Top-level await
+  },
   plugins: [react()]
 })
